ff3509b7b9530f71a7d49cf00611d6a7bf911be2
ryoma okuda <catontheskate4423@gmail.com>
2021-10-04 11:28:00 +0900

    added test code of ex1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # repo for pl04 by prof. fayolle class, c++ programming
+
+- I am Ryoma Okuda and my student id is: s1270174
+
+- email contact from: s1270174@u-aizu.ac.jp
---
+++ ex1/Question 1/intStr_main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(void) {
+
+    int test_int = 1270174;
+    char* casted_string = intToStr(test_int);
+    int casted_int = strToInt(casted_string);
+
+    std::cout << "testing value: " << test_int << std::endl;
+    std::cout << "casted string value using intToStr: " << casted_string << std::endl;
+    std::cout << "casted int value using strToInt: " << casted_int << std::endl;
+    if (test_int == casted_int) {
+        std::cout << "-! test passed." << std::endl;
+    } else {
+        std::cout << "-! test failed. test_int != casted_int" << std::endl;
+    }
+
+}
