017a4fb44c0d304988efb64165be61ad1537bb3b
jumango pussu <jumang4423@Ryomas-MBP.elecom>
2021-11-17 17:08:09 +0900

    e10/q2

--- ex10/Question 2/test_evaluate.cpp
+++ ex10/Question 2/test_evaluate.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 signed main(void)
 {
+  assert(evaluate("( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )") == 101.0);
+  assert(evaluate("( ( ( 2.0 * ( 3.0 - 1.0 ) ) - 5.0 ) * 3.0 )") == -3.0);
   assert(evaluate("3 + 5 * 6 - 7 * ( 8 + 5 )") == -58.0);
   assert(evaluate("25.0 * 12.0 / 5.0 - 8.0") == 52.0);
   return 0;
